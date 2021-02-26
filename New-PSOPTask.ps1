c682b0ac9a6354edb19206d4cc2ba9fa52d8370c
adrwh <adrwh>
2021-02-26 21:32:49 +1100

    ready for dist

--- New-PSOPTask.ps1
+++ New-PSOPTask.ps1
@@ -1,46 +1,97 @@
 function New-PSOPTask {
-    param ($list)
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]$TaskName,
+        [Parameter()]
+        [ValidateSet("Backlog", "Parked", "RoadBlocked", "InProgress")]
+        [String]$Status = "Backlog",
+        [Parameter()]
+        [ValidateSet("Immediate", "High", "Normal", "Low")]
+        [String]$Priority = "Normal",
+        [Parameter()]
+        [ValidateSet("Service Desk", "Infrastructure", "Network Operations", "Event Operations", "Security")]
+        [String]$Department,
+        [Parameter()]
+        [String]$Description
 
-    # OpenProject API Key stored in confid data file
-    $config = Import-PowerShellDataFile -Path config.psd1
+    )
 
-    # Create base64 encoded api authentication credentials
-    $cred = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f 'apikey', $config.key)))
+    begin {
 
-    # Create the Invoke-RestMethod request headers
-    $headers = @{Authorization = "Basic $cred" }
+        # OpenProject API Key stored in confid data file
+        $config = Import-PowerShellDataFile -Path config.psd1
 
-    # Loop through and create each task
-    $list.split(",") | ForEach-Object {
+        # Create base64 encoded api authentication credentials
+        $cred = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f 'apikey', $config.key)))
 
-        Write-Verbose -Verbose $_
+        # Create the Invoke-RestMethod request headers
+        $headers = @{"Authorization" = "Basic $cred";"Content-Type" = 'application/json'}
+
+        # Param switches
+        $StatusSwitch = 0
+        switch ($Status) {
+            "Backlog" { $StatusSwitch = 1; Break }
+            "Parked" { $StatusSwitch = 15; Break }
+            "RoadBlocked" { $StatusSwitch = 16; Break }
+            "InProgress" { $StatusSwitch = 7; Break }
+            Default { $StatusSwitch = 1 }
+        }
+        $DepartmentSwitch = 0
+        switch ($Department) {
+            "Service Desk" { $DepartmentSwitch = 1; Break }
+            "Infrastructure" { $DepartmentSwitch = 2; Break }
+            "Network Operations" { $DepartmentSwitch = 3; Break }
+            "Security" { $DepartmentSwitch = 4; Break }
+            Default {}
+        }
+        $PrioritySwitch = 0
+        switch ($Priority) {
+            "Low" { $PrioritySwitch = 7; Break }
+            "Normal" { $PrioritySwitch = 8; Break }
+            "High" { $PrioritySwitch = 9; Break }
+            "Immediate" { $PrioritySwitch = 10; Break }
+            Default { $PrioritySwitch = 8 }
+        }
+
+    }
+    process {
 
         # This is the request body to POST
         $body = @{
-            subject = $_
+            subject = $TaskName.trim() # Clean up the task name
+            description = @{
+                raw = $Description
+            }
             _links  = @{
                 type         = @{href = "/api/v3/types/1" } # Match the task type (Task, Milestone, Bug, etc, with your own requirement)
-                status       = @{href = "/api/v3/statuses/1" } # Match the status (Backlog, Roadblocked, InProgress, Stalled, etc with your own requirement)
-                priority     = @{href = "/api/v3/priorities/8" } # Match the priority (High, Medium, Low, etc, with your own requirement)
-                customField2 = @{href = "/api/v3/custom_options/5" } # If you want to include custom fields, otherwise comment this line out.
+                status       = @{href = "/api/v3/statuses/$StatusSwitch" } # Match the status (Backlog, Roadblocked, InProgress, Stalled, etc with your own requirement)
+                priority     = @{href = "/api/v3/priorities/$PrioritySwitch" } # Match the priority (High, Medium, Low, etc, with your own requirement)
+                customField2 = @{href = "/api/v3/custom_options/$DepartmentSwitch" } # If you want to include custom fields, otherwise comment this line out.
+                customField9 = @{href = "/api/v3/custom_options/116" } # If you want to include custom fields, otherwise comment this line out.
             }
-        } | ConvertTo-Json  # The OpenProject API expects JSON, so convert the object to JSON
+        } | ConvertTo-Json -Compress # The OpenProject API expects JSON, so convert the object to JSON
 
-        # Testing endpoint
+        # Test endpoint using httpbin.org container
         #$Uri = "http://localhost/anything"
 
         # The project specific Work_Packages endpoint.
-        $Uri = "$($config.opProdHost)/api/v3/projects/$($config.projectId)/work_packages/"
-
+        $Uri = -join ($config.opProdHost, "/api/v3/projects/", $config.projectId, "/work_packages/")
+        
+        # Splat the payload
+        $payload = @{
+            Uri = $Uri
+            Headers = $headers
+            Method = "Post"
+            Body = $body
+            Verbose = $false
+        }
         try {
-            Invoke-RestMethod -Uri $Uri -Headers $headers -ContentType 'application/json' -Method Post -Body $body
+            Invoke-RestMethod @payload
+            Write-Verbose "Added $TaskName"
         }
         catch {
             $_
         }
-
-        # Give the API a breather for large lists
-        Start-Sleep -Milliseconds 200
-
     }
 }
